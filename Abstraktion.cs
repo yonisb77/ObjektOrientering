1dbc23a5a4c089d97d8fae0f573c895849ef032f
yonisb77 <yonis_77@hotmail.com>
2025-09-14 10:25:28 +0200

    Abstraktion Metoden

---
+++ Abstraktion.cs
@@ -0,0 +1,24 @@
+﻿namespace Objektorientering
+{
+
+    //abstraktion
+    //Vi skapar en abstrakt klass som heter Fordon som är väldigt simpel och enkel
+
+    public abstract class Fordon
+    {
+        public abstract void Stanna();
+    }
+
+    //Här ärver sub klassen från Fordon klassen
+    //override ersätter den abstrakta metoden
+    public class Bil : Fordon 
+    {
+        public override void Stanna()
+        {
+         
+         Console.WriteLine("Bilen stannar");
+        }
+        //Abstraktion metoden fokuserar och visar bara det som är viktigt
+        // Jag tog ett väldigt enkelt exempes
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Objektorientering
+{
+    public class Car
+    {
+        static void Main(string[] args)
+        {
+           Bil Bil = new Bil();
+               Bil.Stanna();
+        }
+    }
+}
