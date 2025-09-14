d30687313897995230f1632fbb7175f4913fc162
yonisb77 <yonis_77@hotmail.com>
2025-09-14 10:30:51 +0200

    Inkapsling metoden

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace objektorientering4
+{
+    public class Student
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Yonis", "Bashir");
+        }
+    }
+}
