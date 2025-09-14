5546df5400d43954af2033b06faab431dfd4a35a
yonisb77 <yonis_77@hotmail.com>
2025-09-14 10:27:24 +0200

    Polymorpism Metoden

---
+++ Polymorpism.cs
@@ -0,0 +1,40 @@
+﻿namespace Objektorientering1
+{
+    //polymorphism metod
+
+    //virtual innebär huvudklass
+    //override innebär subklass
+
+    // här skapar vi en klass som heter Djur 
+
+    public class Djur
+    {
+        public string name;
+        
+        public virtual void Ljud()
+        {
+            Console.WriteLine("Djuret låter");
+        }
+
+
+        // här skapar vi en subklass som heter Hund som ärver från Djur
+        public class Hund : Djur
+        {
+            public override void Ljud()
+            {
+                Console.WriteLine("hunden skäller");
+            }
+        }
+        // här skapar vi en subklass som heter Fågel som ärver från huvudklassen Djur
+        public class Fågel : Djur
+        {
+            public override void Ljud()
+            {
+                Console.WriteLine("fågeln kvittrar");
+            }
+        }
+        // En metod som kan visa ljud från olika djur
+        // jag tog insparation från dina inspelningar då programmering är nytt för mig
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Objektorientering1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Djur djur1 = new Djur();
+              djur1.Ljud();
+
+             Djur hund1 = new Djur.Hund();
+                hund1.Ljud();
+
+                Djur fågel1 = new Djur.Fågel();
+                fågel1.Ljud();
+        }
+    }
+}
